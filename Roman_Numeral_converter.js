7222fec6093822d52189dc1eb883f0ccf64518d2
ismail <83200379+ismail3080@users.noreply.github.com>
2023-01-31 04:07:09 +0100

    Add files via upload

---
+++ Roman_Numeral_converter.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+    const decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    const romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+    let result = '';
+  
+    for (let i = 0; i < decimalValue.length; i++) {
+      while (decimalValue[i] <= num) {
+        result += romanNumeral[i];
+        num -= decimalValue[i];
+      }
+    }
+  
+    return result;
+  }
